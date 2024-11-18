fd463304c39ea9a6decddafae520b4743adbd7d1
Choi <wldnd12223@gmail.com>
2024-11-19 00:23:13 +0900

    update kakao

--- js/footer.js
+++ js/footer.js
@@ -5,7 +5,7 @@ footer.innerHTML = `
 
 <div class="bottom">
         <footer>
-            <img src="../img/lic_favicon.png" style="width:40px;background:#fff;"><span id="footer-title">국가 자격증 지원 센터</span><br><br>
+            <img src="../img/lic_favicon.png" style="width:40px;background:#fff;"><span id="footer-title">부업 자격증 지원센터</span><br><br>
             한국직업교육원 | 마케팅 업체: 플로스 | 사업자번호: 471-09-01112 | 주소: 서울 영등포구 선유로 53길 22 502호 | 제휴 문의 : flosmarketing@naver.com<br>
             FLOS (C) All rights reserved
         </footer>
